53dff8a22a6925289920d3bd6610fdce2db08655
Konstantin Zhigaylo <kostyantop8@gmail.com>
2023-09-14 20:53:55 +0300

    docs: add comments in `kamiya_database`

--- kamiya_database/src/lib.rs
+++ kamiya_database/src/lib.rs
@@ -1,5 +1,6 @@
 use serde::{Deserialize, Serialize};
 
+// Note structure.
 #[derive(Serialize, Deserialize, Default, Clone)]
 pub struct Note {
     pub name: String,
@@ -8,11 +9,13 @@ pub struct Note {
     pub description: String,
 }
 
+// Kamiya database structure.
 #[derive(Serialize, Deserialize, Default)]
 pub struct Database {
     notes: Vec<Note>,
 }
 
+// Errors which may occur. 
 #[derive(Debug)]
 pub enum DatabaseError {
     NoteNotFound,
@@ -20,10 +23,12 @@ pub enum DatabaseError {
 }
 
 impl Database {
+    // Check if note exists.
     pub fn note_exists(&self, name: &str) -> bool {
         self.notes.iter().any(|item| item.name == *name.to_owned())
     }
 
+    // Remove note from database.
     pub fn remove_note(&mut self, name: &str) -> Result<(), DatabaseError> {
         match self.get_note_index(name) {
             Ok(index) => {
@@ -34,18 +39,17 @@ impl Database {
         }
     }
 
+    // Get vector of notes from database.
     pub fn get_notes(&self) -> Vec<Note> {
         self.notes.clone()
     }
 
+    // Add new note to the database.
     pub fn add_note(&mut self, new_note: Note) {
         self.notes.push(new_note);
     }
 
-    pub fn notes_count(&self) -> usize {
-        self.notes.len()
-    }
-
+    // Set new name for the note.
     pub fn set_note_name(&mut self, note_name: &str, new_name: &str) -> Result<(), DatabaseError> {
         match self.get_note_index(note_name) {
             Ok(index) => {
@@ -56,6 +60,7 @@ impl Database {
         }
     }
 
+    // Set content for note.
     pub fn set_note_content(
         &mut self,
         note_name: &str,
@@ -70,6 +75,7 @@ impl Database {
         }
     }
 
+    // Set description for note.
     pub fn set_note_description(
         &mut self,
         note_name: &str,
@@ -84,6 +90,7 @@ impl Database {
         }
     }
 
+    // Get note index by name.
     pub fn get_note_index(&self, name: &str) -> Result<usize, DatabaseError> {
         match self
             .notes
@@ -95,6 +102,7 @@ impl Database {
         }
     }
 
+    // Get note by name.
     pub fn get_note(&self, name: &str) -> Result<Note, DatabaseError> {
         match self.get_note_index(name) {
             Ok(index) => match self.notes.get(index) {
@@ -105,6 +113,7 @@ impl Database {
         }
     }
 
+    // Generate new name from template.
     pub fn generate_name(&self, template: &str) -> Result<String, DatabaseError> {
         if !template.contains("&i") {
             return Err(DatabaseError::BadTemplate);
